c5e2372a3b272ebcece2a40b1d936791b4ae0009
kenleytomlin <3004590+kenleytomlin@users.noreply.github.com>
2025-08-12 11:23:31 +0100

    chore(db): set min_pg_version to 14 and install ash-functions extension
    - Updates Repo.installed_extensions/0 and adds min_pg_version/0
    
    - Generates and applies extension migration (dev/test)
    
    - Tests remain green

--- lib/greenup/repo.ex
+++ lib/greenup/repo.ex
@@ -5,6 +5,13 @@ defmodule Greenup.Repo do
   PostgreSQL extensions required by Ash/AshPostgres migrations.
   """
   def installed_extensions do
-    ["uuid-ossp"]
+    ["uuid-ossp", "ash-functions"]
+  end
+
+  @doc """
+  Minimum supported PostgreSQL version for this repo.
+  """
+  def min_pg_version do
+    %Version{major: 14, minor: 0, patch: 0}
   end
 end
